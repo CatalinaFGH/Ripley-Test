a3fe873c95ab2a49fc6f171b1319418c2ca6585f
unknown <catalinagonzalez1410@gmail.com>
2021-05-09 21:18:50 -0300

    agregado el segundo carrusel desktop

--- Prueba_Front-End/src/script.js
+++ Prueba_Front-End/src/script.js
@@ -70,3 +70,75 @@ nextBtn.addEventListener('click', moveToNextSlide);
 prevBtn.addEventListener('click', moveToPreviousSlide);
 
 startSlide();
+
+//SEGUNDO CARRUSEL
+const slideContainer2 = document.querySelector('.best-container');
+const slide2 = document.querySelector('.best-slides');
+const nextBtn2 = document.getElementById('bestNextBtn');
+const prevBtn2 = document.getElementById('bestPrevBtn');
+const interval2 = 3000;
+
+let slides2 = document.querySelectorAll('.best-slide');
+let index2 = 1;
+let slideId2;
+
+const firstClone2 = slides2[0].cloneNode(true);
+const lastClone2 = slides2[slides2.length - 1].cloneNode(true);
+
+firstClone2.id = 'firstClone2';
+lastClone2.id = 'lastClone2';
+
+slide2.append(firstClone2);
+slide2.prepend(lastClone2);
+
+const slideWidth2 = slides2[index2].clientWidth;
+
+slide2.style.transform = `translateX(${-slideWidth2 * index2}px)`;
+
+const startSlide2 = () => {
+  slideId2 = setInterval(() => {
+    moveToNextSlide2();
+  }, interval2);
+};
+
+const getSlides2 = () => document.querySelectorAll('.best-slide');
+
+slide2.addEventListener('transitionend', () => {
+  slides2 = getSlides2();
+  if (slides2[index2].id === firstClone2.id) {
+    slide2.style.transition = 'none';
+    index2 = 1;
+    slide2.style.transform = `translateX(${-slideWidth2 * index2}px)`;
+  }
+
+  if (slides2[index2].id === lastClone2.id) {
+    slide2.style.transition = 'none';
+    index2 = slides2.length - 2;
+    slide2.style.transform = `translateX(${-slideWidth2 * index2}px)`;
+  }
+});
+
+const moveToNextSlide2 = () => {
+  slides2 = getSlides2();
+  if (index2 >= slides2.length - 1) return;
+  index2++;
+  slide2.style.transition = '.7s ease-out';
+  slide2.style.transform = `translateX(${-slideWidth2 * index2}px)`;
+};
+
+const moveToPreviousSlide2 = () => {
+  if (index2 <= 0) return;
+  index2--;
+  slide2.style.transition = '.7s ease-out';
+  slide2.style.transform = `translateX(${-slideWidth2 * index2}px)`;
+};
+
+slideContainer2.addEventListener('mouseenter', () => {
+  clearInterval(slideId2);
+});
+
+slideContainer2.addEventListener('mouseleave', startSlide2);
+nextBtn2.addEventListener('click', moveToNextSlide2);
+prevBtn2.addEventListener('click', moveToPreviousSlide2);
+
+startSlide2();
